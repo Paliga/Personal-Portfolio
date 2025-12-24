a2b262bc315766dc444cba7e02076a06ca60af29
Paliga <80478964+Paliga@users.noreply.github.com>
2025-12-24 09:34:18 +0200

    implemented currently card

--- src/app/components/Currently.tsx
+++ src/app/components/Currently.tsx
@@ -5,12 +5,13 @@ export default function Currently() {
   return (
     <motion.section
       id="currently"
-      className="lg:col-start-5 lg:col-end-7 lg:row-start-4 lg:row-end-5 bg-zinc-900/80 backdrop-blur-xl border border-zinc-800 rounded-2xl p-6 shadow-2xl hover:border-red-500/50 transition-all duration-500"
+      className="lg:col-start-5 lg:col-end-7 lg:row-start-4 lg:row-end-5 bg-zinc-900/80 backdrop-blur-xl border border-zinc-800 rounded-2xl p-5 shadow-2xl hover:border-red-500/50 transition-all duration-500"
       whileHover={{ scale: 1.02 }}
     >
-      <div className="container mx-auto px-6">
-        <h2 className="text-4xl font-bold mb-8">Currently</h2>
+      <div className="container">
+        <h2 className="text-lg font-bold text-red-500 mb-1">Currently</h2>
       </div>
+      <p className='text-sm'>Almost fulfilled military services</p>
     </motion.section>
   );
 }
